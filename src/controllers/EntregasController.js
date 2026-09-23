05fa350e723bdb757df12b559e959b8a73ed96a7
GigoShamak <iago2004.lopes@gmail.com>
2026-09-23 15:33:04 -0300

    feat(controllers): adiciona EntregasController

---
+++ src/controllers/EntregasController.js
@@ -0,0 +1,18 @@
+// Traduz HTTP <-> service. Sem regra de negócio.
+export class EntregasController {
+  constructor(service) {
+    this.service = service;
+  }
+
+  criar = (req, res) => {
+    res.status(201).json(this.service.criar(req.body));
+  };
+
+  listar = (req, res) => {
+    res.status(200).json(this.service.listar());
+  };
+
+  buscarPorId = (req, res) => {
+    res.status(200).json(this.service.buscarPorId(req.params.id));
+  };
+}
